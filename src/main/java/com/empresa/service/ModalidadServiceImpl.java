88340e6e70a5bf7197b4bb6658a9efbba8c63d37
estiben21 <estibenyerovim@gmail.com>
2023-10-21 13:02:56 -0500

    semana 9

--- src/main/java/com/empresa/service/ModalidadServiceImpl.java
+++ src/main/java/com/empresa/service/ModalidadServiceImpl.java
@@ -44,7 +44,22 @@ public class ModalidadServiceImpl implements ModalidadService{
 	public List<Modalidad> buscaPorDeporteEnSede(String sede, int idDeporte) {
 		return repository.buscaPorDeporteEnSede(sede, idDeporte);
 	}
+	@Override
+
+	public List<Modalidad> listaPorNombreIgualRegistra(String nombre) {
+
+		return repository.listaPorNombreIgualRegistra(nombre);
+
+	}
 
 
+
+	@Override
+
+	public List<Modalidad> listaPorNombreIgualActualiza(String nombre, int id) {
+
+		return repository.listaPorNombreIgualActualiza(nombre, id);
+
+	}
 
 }
--- src/main/java/com/empresa/service/ProveedorServiceImpl.java
+++ src/main/java/com/empresa/service/ProveedorServiceImpl.java
@@ -1,5 +1,7 @@
 package com.empresa.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,4 +19,21 @@ public class ProveedorServiceImpl implements ProveedorService{
 		return repository.save(obj);
 	}
 
+	@Override
+
+	public List<Proveedor> listaPorNombreIgual(String nombre) {
+
+		return repository.listaPorNombreIgual(nombre);
+
+	}
+
+
+
+	@Override
+
+	public List<Proveedor> listaPorDniIgual(String dni) {
+
+		return repository.listaPorDniIgual(dni);
+
+	}
 }
